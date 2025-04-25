0101c37359d81b948b93d1d18c8bcf9c92c1d249
utof <bondar030@mail.ru>
2025-04-25 14:40:12 +0300

    add github pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: "/procrastinotion",
+
+  // This generates static HTML/CSS/JS files suitable for GH Pages
+  output: "export",
 };
 
 export default nextConfig;
